c78421a81d645f08fd9a87145d746d3c66e50669
Kevin Lyons <zinckev@gmail.com>
2021-06-16 16:54:12 -0700

    working on 5 day forcast and animation

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ const api = {
 function App() {
   const [query, setQuery] = useState("");
   const [weather, setWeather] = useState({});
+  const [forcast, setForcast] = useState([]);
 
   const search = (e) => {
     if (e.key === "Enter") {
@@ -15,8 +16,16 @@ function App() {
         .then((res) => res.json())
         .then((result) => {
           setWeather(result);
-          setQuery("");
+
           console.log(result);
+        });
+
+      fetch(`${api.base}forecast?q=${query}&units=metric&APPID=${api.key}`)
+        .then((res) => res.json())
+        .then((fourcats) => {
+          setForcast(fourcats.list.filter((e, i) => i % 8 === 0));
+          setQuery("");
+          console.log(forcast);
         });
     }
   };
@@ -79,6 +88,22 @@ function App() {
             <div className="weather-box">
               <div className="temp">{Math.round(weather.main.temp)}°</div>
               <div className="weather">{weather.weather[0].main}</div>
+            </div>
+            <br />
+            <div className="forcast-text"> 5-day forcast</div>
+            <div className="fiveDay">
+              {forcast.map((fourcats) => (
+                <div key={fourcats.dt}>
+                  <div className="forcast-box">
+                    <div className="day">
+                      {fourcats.dt_txt}
+                      <div className="fiveDayTemp">
+                        {Math.round(fourcats.main.temp)}°
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              ))}
             </div>
           </div>
         ) : (
